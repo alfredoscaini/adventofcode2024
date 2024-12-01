d7b2b0628a39c72ede08584d65e205019e9239d6
Alfredo Scaini <alfredo.scaini@gmail.com>
2024-12-01 04:58:49 -0500

    Day 01 of AoC 2024

---
+++ src/01/index.php
@@ -0,0 +1,48 @@
+<?php 
+
+require_once('data.php');
+
+$list_left  = [];
+$list_right = [];
+
+foreach ($data as $list) {
+  $list_left[]  = $list[0];
+  $list_right[] = $list[1];
+}
+
+
+// Q1: Sort the two lists and compare how far apart the value on the left is to the value on the right
+// watch out for negative numbers and get the absolute separation.
+sort($list_left);
+sort($list_right);
+
+$total_difference = 0; // calculate the difference between each set of compared values.
+for ( $i = 0; $i < count($list_left); $i++) {
+  $left  = (count($list_left) > $i)  ? $list_left[$i]  : -1;
+  $right = (count($list_right) > $i) ? $list_right[$i] : -1;
+
+  if ($left !== -1 && $right !== -1) {
+    $total_difference += abs($right - $left);
+  }
+}
+
+print '<p>Q1: The sum of all distances is ' . $total_difference . '</p>'; // 2176849
+
+// Q2: Find out how many times the left column appears in the right column and record the total number of 
+// occurrences. Then multiply this value by the left column value to obtain the total similiarities.
+$counts = [];
+foreach ($list_left as $l_id) {
+  $counts[$l_id] = 0;
+    foreach ($list_right as $r_id) {
+      if ($l_id == $r_id) {
+        $counts[$l_id]++;
+      }
+    }
+}
+
+$total_similarity = 0;
+foreach ($counts as $key => $value) {
+  $total_similarity += ($key * $value);
+}
+
+print '<p>Q2: The sum of all similiarities is ' . $total_similarity . '</p>'; // 23384288
